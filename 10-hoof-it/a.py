1b819bbaf747e9926e6b8532e1cffc738162cf81
Arif Kalluru <arifkalluru@gmail.com>
2024-12-10 23:11:28 +0530

    Complete day 10, move day 1-9 to 01-09

---
+++ 10-hoof-it/a.py
@@ -0,0 +1,40 @@
+# Return: set: A set of strings representing coordinates of unique reachable 9s
+def solve(x, y, n, m, topograph, memo):
+    if memo[x][y] is not None: # Return cached result
+        return memo[x][y]
+
+    if topograph[x][y] == 9: # Endpoint
+        memo[x][y] = {f"{x}{y}"}
+        return memo[x][y]
+
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)] # up, right, down, left
+    memo[x][y] = set()
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m and topograph[nx][ny] == topograph[x][y] + 1:
+            memo[x][y].update(solve(nx, ny, n, m, topograph, memo))
+    
+    return memo[x][y]
+
+def main():
+    topograph = []
+    zeros = []
+
+    with open("input.txt", "r") as file:
+        for i, line in enumerate(file):
+            row = list(map(int, line.strip()))
+            topograph.append(row)
+            zeros.extend((i, j) for j, num in enumerate(row) if num == 0)
+    
+    n, m = len(topograph), len(topograph[0])
+    memo = [[None for _ in range(m)] for _ in range(n)] # memoization table
+    total_count = 0
+
+    for x, y in zeros:
+        total_count += len(solve(x, y, n, m, topograph, memo))
+    
+    print(total_count)
+
+if __name__ == '__main__':
+    main()
